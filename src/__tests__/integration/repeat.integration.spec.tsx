93c55d0cf2bffeb7ff3868cc6efd444276776500
이동아 <christar99@naver.com>
2024-11-10 21:16:10 +0900

    ✨ feature: overlay-kit 추가, server.js 수정, 통합테스트파일 이동, 추가

---
+++ src/__tests__/integration/repeat.integration.spec.tsx
@@ -0,0 +1,31 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { render, screen } from '@testing-library/react';
+import userEvent, { UserEvent } from '@testing-library/user-event';
+import { ReactElement } from 'react';
+
+// ! Hard 여기 제공 안함
+const setup = (element: ReactElement) => {
+  const user = userEvent.setup();
+
+  return { ...render(<ChakraProvider>{element}</ChakraProvider>), user }; // ? Med: 왜 ChakraProvider로 감싸는지 물어보자
+};
+
+// ! Hard 여기 제공 안함
+const saveSchedule = async (
+  user: UserEvent,
+  form: Omit<Event, 'id' | 'notificationTime' | 'repeat'>
+) => {
+  const { title, date, startTime, endTime, location, description, category } = form;
+
+  await user.click(screen.getAllByText('일정 추가')[0]);
+
+  await user.type(screen.getByLabelText('제목'), title);
+  await user.type(screen.getByLabelText('날짜'), date);
+  await user.type(screen.getByLabelText('시작 시간'), startTime);
+  await user.type(screen.getByLabelText('종료 시간'), endTime);
+  await user.type(screen.getByLabelText('설명'), description);
+  await user.type(screen.getByLabelText('위치'), location);
+  await user.selectOptions(screen.getByLabelText('카테고리'), category);
+
+  await user.click(screen.getByTestId('event-submit-button'));
+};
