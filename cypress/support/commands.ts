3f577f3641b18f2e5507f902291f1a6d920ee1c2
rnjsguswl39 <rnjsguswl39@gmail.com>
2024-11-12 00:51:42 +0900

    test:  삭제 시나리오 테스트 추가

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,5 +1,24 @@
 import '@testing-library/cypress/add-commands';
 
+Cypress.Commands.add('fillEventForm', (event) => {
+  cy.findByLabelText('제목').type(event.title);
+  cy.findByLabelText('날짜').type(event.date);
+  cy.findByLabelText('시작 시간').type(event.startTime);
+  cy.findByLabelText('종료 시간').type(event.endTime);
+  cy.findByLabelText('위치').type(event.location);
+  cy.findByLabelText('설명').type(event.description);
+  cy.findByLabelText('카테고리').select(event.category);
+
+  if (event.isRepeating) {
+    cy.findByLabelText('반복 일정').check();
+    cy.findByLabelText('반복 유형').select(event.repeatType);
+    cy.findByLabelText('반복 간격').clear().type(`{selectall}${event.repeatInterval}`);
+    cy.findByLabelText('반복 종료일').type(event.repeatEndDate);
+  }
+
+  cy.findByLabelText('알림 설정').select(event.notificationTime);
+});
+
 /// <reference types="cypress" />
 // ***********************************************
 // This example commands.ts shows you how to
