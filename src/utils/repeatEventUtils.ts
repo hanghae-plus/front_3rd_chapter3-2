43931e7a30346be3e0cb199e30ea580730901bfe
이동아 <christar99@naver.com>
2024-11-12 02:12:10 +0900

    test: reatEventUtils 생성, 실패테스트 추가

---
+++ src/utils/repeatEventUtils.ts
@@ -0,0 +1,8 @@
+import { Event, RepeatEvent } from '../types';
+
+export const getRepeatingEvent = (events: Event[]): RepeatEvent => {
+  return {};
+};
+export const getMaxRepeatLength = (event: Event) => {
+  return 9999;
+};
