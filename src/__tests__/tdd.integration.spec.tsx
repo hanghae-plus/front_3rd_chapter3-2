ef6ff50ec036c6fe44f49a3f840add6897220fdd
khjbest39 <khjbest39@golfzon.com>
2024-11-11 20:08:31 +0900

    fix: 테스트코드 불필요 import 제거

--- src/__tests__/tdd.integration.spec.tsx
+++ src/__tests__/tdd.integration.spec.tsx
@@ -1,55 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { render, screen, within, act } from '@testing-library/react';
-import { UserEvent, userEvent } from '@testing-library/user-event';
+import { render, screen, within } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { http, HttpResponse } from 'msw';
 import { ReactElement } from 'react';
-import { Mock } from 'vitest';
 
-import {
-  setupMockHandlerCreation,
-  setupMockHandlerDeletion,
-  setupMockHandlerUpdating,
-} from '../__mocks__/handlersUtils';
+import { setupMockHandlerCreation } from '../__mocks__/handlersUtils';
 import App from '../App';
-import { useEventForm } from '../hooks/useEventForm';
 import { server } from '../setupTests';
 import { Event } from '../types';
 
-// ! Hard 여기 제공 안함
 const setup = (element: ReactElement) => {
   const user = userEvent.setup();
 
   return { ...render(<ChakraProvider>{element}</ChakraProvider>), user }; // ? Med: 왜 ChakraProvider로 감싸는지 물어보자
-};
-
-// ! Hard 여기 제공 안함
-const saveSchedule = async (user: UserEvent, form: Event) => {
-  const {
-    title,
-    date,
-    startTime,
-    endTime,
-    location,
-    description,
-    category,
-    notificationTime,
-    repeat,
-  } = form;
-
-  await user.click(screen.getAllByText('일정 추가')[0]);
-
-  await user.type(screen.getByLabelText('제목'), title);
-  await user.type(screen.getByLabelText('날짜'), date);
-  await user.type(screen.getByLabelText('시작 시간'), startTime);
-  await user.type(screen.getByLabelText('종료 시간'), endTime);
-  await user.type(screen.getByLabelText('설명'), description);
-  await user.type(screen.getByLabelText('위치'), location);
-  await user.selectOptions(screen.getByLabelText('카테고리'), category);
-  await user.selectOptions(screen.getByLabelText('반복 유형'), repeat.type);
-  await user.type(screen.getByLabelText('반복 간격'), repeat.interval.toString());
-  await user.type(screen.getByLabelText('반복 종료일'), repeat.endDate ?? '');
-
-  await user.click(screen.getByTestId('event-submit-button'));
 };
 
 // 1. 반복 유형 선택
