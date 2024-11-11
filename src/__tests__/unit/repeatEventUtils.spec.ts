69753abadc935bc5ea5aba3a45d936d7a45b7780
이동아 <christar99@naver.com>
2024-11-12 03:56:08 +0900

    ✅ test: repeatEventUtils함수 구현,  tdd 성공테스트 완료

--- src/__tests__/unit/repeatEventUtils.spec.ts
+++ src/__tests__/unit/repeatEventUtils.spec.ts
@@ -1,5 +1,5 @@
 import { Event, RepeatEvent } from '../../types';
-import { getMaxRepeatLength, getRepeatingEvent } from '../../utils/repeatEventUtils';
+import { getRepeatingEvent } from '../../utils/repeatEventUtils';
 
 describe('getRepeatingEvent', () => {
   it('RepeatEvent 타입의 배열을 반환한다.', () => {
@@ -17,67 +17,71 @@ describe('getRepeatingEvent', () => {
         notificationTime: 1,
       },
     ];
-    const repeatingEvent: RepeatEvent = getRepeatingEvent(events);
+    const repeatingEvent: RepeatEvent[] = getRepeatingEvent(events);
 
-    expect(repeatingEvent.event).toHaveLength(9);
-    expect(repeatingEvent.eventId).toBe('2b7545a6-ebee-426c-b906-2329bc8d62bd');
-    expect(repeatingEvent.type).toBe('daily');
-    expect(repeatingEvent.event[8].date).toBe('2024-11-28');
+    expect(repeatingEvent[0].event).toHaveLength(9);
+    expect(repeatingEvent[0].eventId).toBe('2b7545a6-ebee-426c-b906-2329bc8d62bd');
+    expect(repeatingEvent[0].type).toBe('daily');
+    expect(repeatingEvent[0].event[8].date).toBe('2024-11-28');
   });
-});
 
-describe('getMaxRepeatLength', () => {
-  it('endDate가 공백일 때(무제한) 반복일정의 최대개수는 40개이다.', () => {
-    const event: Event = {
-      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
-      title: '팀 회의',
-      date: '2024-11-20',
-      startTime: '10:00',
-      endTime: '11:00',
-      description: '주간 팀 미팅',
-      location: '회의실 A',
-      category: '업무',
-      repeat: { type: 'weekly', interval: 1 },
-      notificationTime: 1,
-    };
-    const maxLength = getMaxRepeatLength(event);
+  it('endDate가 공백일 때(무제한) 반복일정의 개수는 40개이다.', () => {
+    const events: Event[] = [
+      {
+        id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+        title: '팀 회의',
+        date: '2024-11-20',
+        startTime: '10:00',
+        endTime: '11:00',
+        description: '주간 팀 미팅',
+        location: '회의실 A',
+        category: '업무',
+        repeat: { type: 'weekly', interval: 1 },
+        notificationTime: 1,
+      },
+    ];
+    const repeatingEvent: RepeatEvent[] = getRepeatingEvent(events);
 
-    expect(maxLength <= 40).toBe(true);
+    expect(repeatingEvent[0].event.length <= 40).toBe(true);
   });
 
   it('endDate가 공백이 아니고 반복일정이 40개를 초과할 때, 40개를 반환한다.', () => {
-    const event: Event = {
-      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
-      title: '팀 회의',
-      date: '2024-11-20',
-      startTime: '10:00',
-      endTime: '11:00',
-      description: '주간 팀 미팅',
-      location: '회의실 A',
-      category: '업무',
-      repeat: { type: 'daily', interval: 1, endDate: '2025-10-10' },
-      notificationTime: 1,
-    };
-    const maxLength = getMaxRepeatLength(event);
+    const events: Event[] = [
+      {
+        id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+        title: '팀 회의',
+        date: '2024-11-20',
+        startTime: '10:00',
+        endTime: '11:00',
+        description: '주간 팀 미팅',
+        location: '회의실 A',
+        category: '업무',
+        repeat: { type: 'daily', interval: 1, endDate: '2025-10-10' },
+        notificationTime: 1,
+      },
+    ];
+    const repeatingEvent: RepeatEvent[] = getRepeatingEvent(events);
 
-    expect(maxLength).toBe(40);
+    expect(repeatingEvent[0].event.length).toBe(40);
   });
 
   it('endDate가 공백이 아니고 반복일정이 40개 이하일 때, 반복일정의 개수를 반환한다.', () => {
-    const event: Event = {
-      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
-      title: '팀 회의',
-      date: '2024-11-20',
-      startTime: '10:00',
-      endTime: '11:00',
-      description: '주간 팀 미팅',
-      location: '회의실 A',
-      category: '업무',
-      repeat: { type: 'daily', interval: 1, endDate: '2024-11-28' },
-      notificationTime: 1,
-    };
-    const maxLength = getMaxRepeatLength(event);
+    const events: Event[] = [
+      {
+        id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+        title: '팀 회의',
+        date: '2024-11-20',
+        startTime: '10:00',
+        endTime: '11:00',
+        description: '주간 팀 미팅',
+        location: '회의실 A',
+        category: '업무',
+        repeat: { type: 'daily', interval: 1, endDate: '2024-11-28' },
+        notificationTime: 1,
+      },
+    ];
+    const repeatingEvent: RepeatEvent[] = getRepeatingEvent(events);
 
-    expect(maxLength).toBe(9);
+    expect(repeatingEvent[0].event.length).toBe(9);
   });
 });
--- src/utils/repeatEventUtils.ts
+++ src/utils/repeatEventUtils.ts
@@ -1,8 +1,44 @@
+import dayjs from 'dayjs';
+
 import { Event, RepeatEvent } from '../types';
 
-export const getRepeatingEvent = (events: Event[]): RepeatEvent => {
-  return {};
+export const getRepeatingEvent = (events: Event[]): RepeatEvent[] => {
+  const repeatEvent = events.map((event) => ({
+    eventId: event.id,
+    type: event.repeat.type,
+    interval: event.repeat.interval,
+    endDate: event.repeat.endDate,
+    event: getEventsFromRepeat(event),
+  }));
+  return repeatEvent;
 };
-export const getMaxRepeatLength = (event: Event) => {
-  return 9999;
+
+const getEventsFromRepeat = (event: Event) => {
+  if (event.repeat.type === 'none') {
+    return [];
+  }
+
+  let currentDate = dayjs(event.date);
+  const endDate = dayjs(event.repeat.endDate || '2100-12-31');
+  const formatTerm = {
+    daily: 'day',
+    weekly: 'week',
+    monthly: 'month',
+    yearly: 'year',
+  };
+  const unit = formatTerm[event.repeat.type] as 'day' | 'week' | 'month' | 'year';
+  const repeatEvent = [];
+
+  while (dayjs(endDate).diff(currentDate, unit) >= 0 && repeatEvent.length < 40) {
+    // eslint-disable-next-line no-unused-vars
+    const { id, repeat, date, ...othersRepeatItem } = event;
+    repeatEvent.push({
+      repeatId: crypto.randomUUID(),
+      date: dayjs(currentDate).format('YYYY-MM-DD'),
+      ...othersRepeatItem,
+    });
+    currentDate = dayjs(currentDate).add(event.repeat.interval, unit);
+  }
+
+  return repeatEvent;
 };
