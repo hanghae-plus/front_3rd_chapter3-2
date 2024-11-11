4fcfdb2fe2ea3106134f9a0deca16dc385735a0c
rnjsguswl39 <rnjsguswl39@gmail.com>
2024-11-12 01:02:34 +0900

    fix:lint error

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents() {
       // Node 이벤트 리스너 구현
     },
     baseUrl: 'http://localhost:5173',
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,4 @@
+/* eslint-disable cypress/unsafe-to-chain-command */
 import '@testing-library/cypress/add-commands';
 
 Cypress.Commands.add('fillEventForm', (event) => {
