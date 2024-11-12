035a66fc05ce69f151120bec772ece8d2d52bedf
이동아 <christar99@naver.com>
2024-11-12 17:09:07 +0900

    ✅ test: useRepeatEvent - '일정을 생성할때 반복일정이 있다면 저장한다' 성공테스트 완료

--- src/__tests__/hooks/useRepeatEvent.spec.ts
+++ src/__tests__/hooks/useRepeatEvent.spec.ts
@@ -15,13 +15,13 @@ it('일정을 생성할때 반복일정이 있다면 저장한다.', async () =>
 
   await act(() => Promise.resolve(null));
 
-  console.log(result.current.repeatEventHook.repeatEvent);
   const { eventId, type, interval, event } = result.current.repeatEventHook.repeatEvent[0];
-  expect(eventId).toBe('2b7545a6-ebee-426c-b906-2329bc8d62bd');
+  expect(eventId).toBe('2ab06561-10f8-4e7f-8128-4b2dd343c6b9');
   expect(type).toBe('monthly');
   expect(interval).toBe(1);
-  expect(event[0].title).toBe('팀회의');
+  expect(event[0].title).toBe('기존 회의');
 });
+
 it('일정에서 반복일정 전체를 수정할 때, 반복일정을 요청(유형, 간격, 종료일)에 맞게 수정한다.', () => {});
 it('일정을 삭제할 때 해당 반복일정도 삭제한다.', () => {});
 it('반복일정 중 하나를 수정하면 그 일정은 단일 일정으로 변경된다.', () => {});
--- src/hooks/useRepeatEvent.ts
+++ src/hooks/useRepeatEvent.ts
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 
 import { Event, RepeatEvent } from '../types';
+import { getRepeatingEvent } from '../utils/repeatEventUtils';
 
 export const useRepeatEvent = () => {
   const [repeatEvent, setRepeatEvent] = useState<RepeatEvent[]>([]);
 
   // 반복일정을 전체 변경
-  const changeRepeatEvent = (events: Event[]) => {};
+  const changeRepeatEvent = (events: Event[]) => {
+    const eventWithRepeat = events.filter((event) => event.repeat.type !== 'none');
+    setRepeatEvent(getRepeatingEvent(eventWithRepeat));
+  };
 
   // 일정의 반복일정 전체를 삭제
   const deleteRepeatEvent = () => {};
