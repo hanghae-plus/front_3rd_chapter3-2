eb549ad414cb08a7ade97a83c44bcead2b8cbfae
cruellaDev <cruellaDevLee@gmail.com>
2024-11-12 13:17:37 +0900

    * init: server 반복 일정에 대한 서버 정보 추가

--- server.js
+++ server.js
@@ -40,21 +40,23 @@ app.put('/api/events/:id', async (req, res) => {
   const events = await getEvents();
   const { id } = req.params;
   const eventIndex = events.events.findIndex((event) => event.id === id);
-  if (eventIndex > -1) {
-    const newEvents = [...events.events];
-    newEvents[eventIndex] = { ...events.events[eventIndex], ...req.body };
 
-    fs.writeFileSync(
-      `${__dirname}/src/__mocks__/response/realEvents.json`,
-      JSON.stringify({
-        events: newEvents,
-      })
-    );
-
-    res.json(events.events[eventIndex]);
-  } else {
+  if (eventIndex === -1) {
     res.status(404).send('Event not found');
+    return;
   }
+
+  const newEvents = [...events.events];
+  newEvents[eventIndex] = { ...events.events[eventIndex], ...req.body };
+
+  fs.writeFileSync(
+    `${__dirname}/src/__mocks__/response/realEvents.json`,
+    JSON.stringify({
+      events: newEvents,
+    })
+  );
+
+  res.json(events.events[eventIndex]);
 });
 
 app.delete('/api/events/:id', async (req, res) => {
@@ -65,6 +67,72 @@ app.delete('/api/events/:id', async (req, res) => {
     `${__dirname}/src/__mocks__/response/realEvents.json`,
     JSON.stringify({
       events: events.events.filter((event) => event.id !== id),
+    })
+  );
+
+  res.status(204).send();
+});
+
+app.post('/api/events-list', async (req, res) => {
+  const events = await getEvents();
+  const repeatId = randomUUID();
+  const newEvents = req.body.events.map((event) => {
+    const isRepeatEvent = event.repeat.type !== 'none';
+    return {
+      id: randomUUID(),
+      ...event,
+      repeat: {
+        ...event.repeat,
+        id: isRepeatEvent ? repeatId : undefined,
+      },
+    };
+  });
+
+  fs.writeFileSync(
+    `${__dirname}/src/__mocks__/response/realEvents.json`,
+    JSON.stringify({
+      events: [...events.events, ...newEvents],
+    })
+  );
+
+  res.status(201).json(newEvents);
+});
+
+app.put('/api/events-list', async (req, res) => {
+  const events = await getEvents();
+  let isUpdated = false;
+
+  const newEvents = [...events.events];
+  req.body.events.forEach((event) => {
+    const eventIndex = events.events.findIndex((target) => target.id === event.id);
+    if (eventIndex > -1) {
+      isUpdated = true;
+      newEvents[eventIndex] = { ...events.events[eventIndex], ...event };
+    }
+  });
+
+  if (isUpdated) {
+    fs.writeFileSync(
+      `${__dirname}/src/__mocks__/response/realEvents.json`,
+      JSON.stringify({
+        events: newEvents,
+      })
+    );
+
+    res.json(events.events);
+  } else {
+    res.status(404).send('Event not found');
+  }
+});
+
+app.delete('/api/events-list', async (req, res) => {
+  const events = await getEvents();
+  const newEvents = events.events.filter((event) => !req.body.eventIds.includes(event.id)); // ? ids를 전달하면 해당 아이디를 기준으로 events에서 제거
+
+  fs.writeFileSync(
+    `${__dirname}/src/__mocks__/response/realEvents.json`,
+    JSON.stringify({
+      events: newEvents,
     })
   );
 
