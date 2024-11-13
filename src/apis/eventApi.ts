2d21ea58b4cdba58f2057f999e599a611590c168
정원 <jwha94@gmail.com>
2024-11-13 21:11:18 +0900

    feat(eventApi): addEventList, editEventList 추가

--- src/apis/eventApi.ts
+++ src/apis/eventApi.ts
@@ -1,24 +1,41 @@
 import { Event, EventForm, EventId } from '../types';
 import { safeFetch } from '../utils/safeFetch';
 
+const api = {
+  events: '/api/events',
+  eventsList: '/api/events-list',
+};
+
+type EventsResponse = { events: Event[] };
+
 export const eventApi = {
   fetchEvents: async () => {
-    const { events } = await safeFetch<{ events: Event[] }>('/api/events');
+    const { events } = await safeFetch<EventsResponse>(api.events);
     return events;
   },
 
-  addEvent: async (eventForm: EventForm | Event) => {
-    const response = await safeFetch.post<Event>('/api/events', eventForm);
+  addEvent: async (eventForm: EventForm) => {
+    const response = await safeFetch.post<Event>(api.events, eventForm);
     return response;
   },
 
   editEvent: async (event: Event) => {
-    const response = await safeFetch.put<Event>(`/api/events/${event.id}`, event);
+    const response = await safeFetch.put<Event>(`${api.events}/${event.id}`, event);
     return response;
   },
 
   deleteEvent: async (id: EventId) => {
-    const response = await safeFetch.delete(`/api/events/${id}`);
+    const response = await safeFetch.delete(`${api.events}/${id}`);
+    return response;
+  },
+
+  addEventList: async (events: EventForm[]) => {
+    const response = await safeFetch.post<EventsResponse>(api.eventsList, { events });
+    return response;
+  },
+
+  editEventList: async (events: Event[]) => {
+    const response = await safeFetch.put<EventsResponse>(api.eventsList, { events });
     return response;
   },
 };
