4ea4324f07b191ef2b9c3f7ce5542bfee9e6f0cc
sanghyuk-2i <desummit1@gmail.com>
2024-11-13 10:54:58 +0900

    test: 반복 일정 생성에 관련 테스트 코드 작성
    - 예상되는 유틸 함수들 초기 선언
    - 반복 일정 생성 관련 테스트 description만 작성

---
+++ src/utils/eventRepeat.ts
@@ -0,0 +1,11 @@
+// 반복 일정 관련 Utils
+
+function createRepeatingAnnualEvents() {}
+
+function createRepeatingMonthlyEvents() {}
+
+function createRepeatingWeeklyEvents() {}
+
+function createRepeatingDayEvents() {}
+
+export function getRepeatEvents() {}
