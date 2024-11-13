ea53450a90ac81f9c453d33b19fbbf5b9981ed5f
이동아 <christar99@naver.com>
2024-11-13 16:37:10 +0900

    ✨ feature: repeatEventUtils 로직 구현

--- src/utils/repeatEventUtils.ts
+++ src/utils/repeatEventUtils.ts
@@ -2,6 +2,43 @@ import dayjs from 'dayjs';
 
 import { Event } from '../types';
 
-export const getRepeatingEvent = (events: Event[]) => {};
+const formatRepeatType = {
+  daily: 'day',
+  weekly: 'week',
+  monthly: 'month',
+  yearly: 'year',
+};
 
-const getRepeatingUntilEnd = (events: Event) => {};
+export const getRepeatingEvent = (events: Event[]): Array<Event[]> => {
+  const eventsWithRepeat = events.filter((event) => event.repeat.type !== 'none');
+  const repeatingEvents = eventsWithRepeat.map((event) => getRepeatingUntilEnd(event));
+
+  return repeatingEvents;
+};
+
+const getRepeatingUntilEnd = (event: Event) => {
+  if (event.repeat.type === 'none') {
+    return [];
+  }
+
+  let currentDate = event.date;
+  const endDate = event.repeat.endDate || '2025-06-30';
+  let repeatInterval = event.repeat.interval;
+  const repeatType = formatRepeatType[event.repeat.type] as 'day' | 'week' | 'month' | 'year';
+  const repeatEvents = [];
+
+  while (isBeforeEndDate(currentDate, endDate)) {
+    repeatEvents.push({
+      ...event,
+      date: currentDate,
+    });
+    currentDate = dayjs(event.date).add(repeatInterval, repeatType).format('YYYY-MM-DD');
+    repeatInterval += event.repeat.interval;
+  }
+
+  return repeatEvents;
+};
+
+const isBeforeEndDate = (targetDate: string, endDate: string) => {
+  return dayjs(targetDate).isBefore(dayjs(endDate)) || targetDate === endDate;
+};
