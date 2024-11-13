9362e2a8c9cb278f160b549b28a714d2d4045fe5
이동아 <christar99@naver.com>
2024-11-13 12:16:31 +0900

    ✅ test: repeatEventUtil 테스트, 함수 리뉴얼

--- src/__tests__/unit/repeatEventUtils.spec.ts
+++ src/__tests__/unit/repeatEventUtils.spec.ts
@@ -1,87 +1,12 @@
-import { Event, RepeatEvent } from '../../types';
+// repeatEvent를 date, interval에 맞게 추출해내는 로직을 만들어야함.
+
+import { Event } from '../../types';
 import { getRepeatingEvent } from '../../utils/repeatEventUtils';
 
 describe('getRepeatingEvent', () => {
-  it('RepeatEvent 타입의 배열을 반환한다.', () => {
-    const events: Event[] = [
-      {
-        id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
-        title: '팀 회의',
-        date: '2024-11-20',
-        startTime: '10:00',
-        endTime: '11:00',
-        description: '주간 팀 미팅',
-        location: '회의실 A',
-        category: '업무',
-        repeat: { type: 'daily', interval: 1, endDate: '2024-11-28' },
-        notificationTime: 1,
-      },
-    ];
-    const repeatingEvent: RepeatEvent[] = getRepeatingEvent(events);
+  it('repeat.type이 none이 아닌 경우에만 반환한다.', () => {});
 
-    expect(repeatingEvent[0].event).toHaveLength(9);
-    expect(repeatingEvent[0].eventId).toBe('2b7545a6-ebee-426c-b906-2329bc8d62bd');
-    expect(repeatingEvent[0].type).toBe('daily');
-    expect(repeatingEvent[0].event[8].date).toBe('2024-11-28');
-  });
+  it('repeat.eventDate속성이 없을 경우, 반환값의 date는 2025-06-30을 넘기지 않는다.', () => {});
 
-  it('endDate가 공백일 때(무제한) 반복일정의 개수는 40개이다.', () => {
-    const events: Event[] = [
-      {
-        id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
-        title: '팀 회의',
-        date: '2024-11-20',
-        startTime: '10:00',
-        endTime: '11:00',
-        description: '주간 팀 미팅',
-        location: '회의실 A',
-        category: '업무',
-        repeat: { type: 'weekly', interval: 1 },
-        notificationTime: 1,
-      },
-    ];
-    const repeatingEvent: RepeatEvent[] = getRepeatingEvent(events);
-
-    expect(repeatingEvent[0].event.length <= 40).toBe(true);
-  });
-
-  it('endDate가 공백이 아니고 반복일정이 40개를 초과할 때, 40개를 반환한다.', () => {
-    const events: Event[] = [
-      {
-        id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
-        title: '팀 회의',
-        date: '2024-11-20',
-        startTime: '10:00',
-        endTime: '11:00',
-        description: '주간 팀 미팅',
-        location: '회의실 A',
-        category: '업무',
-        repeat: { type: 'daily', interval: 1, endDate: '2025-10-10' },
-        notificationTime: 1,
-      },
-    ];
-    const repeatingEvent: RepeatEvent[] = getRepeatingEvent(events);
-
-    expect(repeatingEvent[0].event.length).toBe(40);
-  });
-
-  it('endDate가 공백이 아니고 반복일정이 40개 이하일 때, 반복일정의 개수를 반환한다.', () => {
-    const events: Event[] = [
-      {
-        id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
-        title: '팀 회의',
-        date: '2024-11-20',
-        startTime: '10:00',
-        endTime: '11:00',
-        description: '주간 팀 미팅',
-        location: '회의실 A',
-        category: '업무',
-        repeat: { type: 'daily', interval: 1, endDate: '2024-11-28' },
-        notificationTime: 1,
-      },
-    ];
-    const repeatingEvent: RepeatEvent[] = getRepeatingEvent(events);
-
-    expect(repeatingEvent[0].event.length).toBe(9);
-  });
+  it('매월 31, 30일 등에 등록할 경우 그 날짜가 없을때 그 월의 말일에 등록한다.', () => {});
 });
