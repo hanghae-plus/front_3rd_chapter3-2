d85603b2600720d3b58a3653e43d61975142c951
terry.hwang <terry.hwang@h2ohospitality.io>
2024-11-13 20:40:16 +0900

    getRecurringEventList 일 기준 테스트 완료

--- src/feature/addOrEdit/@utils/getRecurringEventList.ts
+++ src/feature/addOrEdit/@utils/getRecurringEventList.ts
@@ -1,1 +1,43 @@
-export function getRecurringEventList() {}
+import { RepeatType } from '../../../types';
+
+type RecurringEventList = {
+  startDate: string;
+  endDate: string;
+  interval: number;
+  type: RepeatType;
+};
+
+export function getRecurringEventList({
+  startDate,
+  endDate,
+  interval,
+  type,
+}: RecurringEventList): string[] {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const recurringDates: string[] = [];
+
+  let currentDate = new Date(start);
+
+  while (currentDate <= end) {
+    recurringDates.push(currentDate.toISOString().split('T')[0]);
+
+    switch (type) {
+      case 'daily':
+        currentDate.setDate(currentDate.getDate() + interval);
+        break;
+      case 'weekly':
+        currentDate.setDate(currentDate.getDate() + interval * 7);
+        break;
+      case 'monthly':
+        currentDate.setMonth(currentDate.getMonth() + interval);
+        break;
+      case 'yearly':
+        currentDate.setFullYear(currentDate.getFullYear() + interval);
+        break;
+      default:
+    }
+  }
+
+  return recurringDates;
+}
