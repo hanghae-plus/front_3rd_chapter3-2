37118011d11907ffb6682330ae5d25b5c5257a6e
codeeon <yeon.develop@gmail.com>
2024-11-15 07:53:05 +0900

    Feat: generateRepeatEventDates weekly

--- src/utils/repeatEventUtils.ts
+++ src/utils/repeatEventUtils.ts
@@ -1,3 +1,43 @@
 import { RepeatInfo } from '../types';
 
-export const generateRepeatEventDates = (baseDate: string, repeat: RepeatInfo) => {};
+const formatDateToString = (date: Date) => date.toISOString().split('T')[0];
+const formatStringToDate = (dateString: string) => new Date(dateString);
+
+const addWeeks = (date: Date, weeks: number) => {
+  const newDate = new Date(date);
+  newDate.setDate(date.getDate() + weeks * 7);
+  return newDate;
+};
+
+export const generateRepeatEventDates = (baseDate: string, repeat: RepeatInfo) => {
+  if (!repeat.type || repeat.type === 'none') {
+    return [baseDate];
+  }
+
+  const stringDateList: string[] = [];
+
+  const startDate = formatStringToDate(baseDate);
+  const endDate = repeat.endDate
+    ? formatStringToDate(repeat.endDate)
+    : formatStringToDate('2025-06-30');
+
+  let currentDate = startDate;
+
+  while (currentDate <= endDate) {
+    stringDateList.push(formatDateToString(currentDate));
+
+    switch (repeat.type) {
+      case 'daily':
+        break;
+      case 'weekly':
+        currentDate = addWeeks(currentDate, repeat.interval);
+        break;
+      case 'monthly':
+        break;
+      case 'yearly':
+        break;
+    }
+  }
+
+  return stringDateList;
+};
