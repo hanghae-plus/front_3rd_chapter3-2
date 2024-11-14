108958f3bfe947e548a58ed640af6e8e16450da6
Bong <borobong230@gmail.com>
2024-11-15 00:26:32 +0900

    :recycle: [Refactor] 기존 테스트 분리(요구사항 별로 묶음) #main

--- src/__tests__/repeat.integration.spec.tsx
+++ src/__tests__/repeat.integration.spec.tsx
@@ -43,67 +43,76 @@ const renderEventForm = (initialValues?: [atom: any, value: any][]) => {
   }
 };
 
-it('반복 일정 미체크 시 반복 유형 옵션이 뜨지 않는다.', async () => {
-  renderEventForm();
+describe('반복 유형 선택', () => {
+  it('반복 일정 미체크 시 반복 유형 옵션이 뜨지 않는다.', async () => {
+    renderEventForm();
 
-  const repeatTypeSelect = screen.queryByLabelText(/반복 유형/);
-  const repeatInterval = screen.queryByLabelText(/반복 간격/);
-  const repeatEnd = screen.queryByLabelText(/반복 종료일/);
+    const repeatTypeSelect = screen.queryByLabelText(/반복 유형/);
+    const repeatInterval = screen.queryByLabelText(/반복 간격/);
+    const repeatEnd = screen.queryByLabelText(/반복 종료일/);
 
-  expect(repeatTypeSelect).not.toBeInTheDocument();
-  expect(repeatInterval).not.toBeInTheDocument();
-  expect(repeatEnd).not.toBeInTheDocument();
-});
+    expect(repeatTypeSelect).not.toBeInTheDocument();
+    expect(repeatInterval).not.toBeInTheDocument();
+    expect(repeatEnd).not.toBeInTheDocument();
+  });
 
-it('반복 일정 체크 시 반복 유형 옵션이 뜬다.', async () => {
-  renderEventForm();
+  it('반복 일정 체크 시 반복 유형 옵션이 뜬다.', async () => {
+    renderEventForm();
 
-  const repeatCheckbox = screen.getByRole('checkbox', { name: /반복 일정/ });
-  await userEvent.click(repeatCheckbox);
+    const repeatCheckbox = screen.getByRole('checkbox', { name: /반복 일정/ });
+    await userEvent.click(repeatCheckbox);
 
-  const repeatTypeSelect = screen.getByLabelText(/반복 유형/);
-  const repeatInterval = screen.getByLabelText(/반복 간격/);
-  const repeatEnd = screen.getByLabelText(/반복 종료일/);
+    const repeatTypeSelect = screen.getByLabelText(/반복 유형/);
+    const repeatInterval = screen.getByLabelText(/반복 간격/);
+    const repeatEnd = screen.getByLabelText(/반복 종료일/);
 
-  expect(repeatTypeSelect).toBeInTheDocument();
-  expect(repeatInterval).toBeInTheDocument();
-  expect(repeatEnd).toBeInTheDocument();
-});
+    expect(repeatTypeSelect).toBeInTheDocument();
+    expect(repeatInterval).toBeInTheDocument();
+    expect(repeatEnd).toBeInTheDocument();
+  });
 
-it('반복 간격을 0으로 작성 후 일정 추가 시 반복 간격을 1 이상의 숫자로 입력해주세요 문구가 뜬다', async () => {
-  renderEventForm();
+  it('반복 유형 SELECT 옵션이 모두 표시된다.(매일/매주/매월/매년 옵션이 있어야 함)', async () => {
+    renderEventForm();
 
-  await userEvent.type(screen.getByLabelText(/제목/), '제목');
-  await userEvent.type(screen.getByLabelText(/날짜/), '2024-11-13');
-  await userEvent.type(screen.getByLabelText(/시작 시간/), '09:00');
-  await userEvent.type(screen.getByLabelText(/종료 시간/), '10:00');
-  await userEvent.type(screen.getByLabelText(/설명/), '설명');
-  await userEvent.type(screen.getByLabelText(/위치/), '위치');
-  await userEvent.selectOptions(screen.getByLabelText(/카테고리/), '업무');
+    await userEvent.click(screen.getByRole('checkbox', { name: /반복 일정/ }));
 
-  await userEvent.click(screen.getByRole('checkbox', { name: /반복 일정/ }));
+    const select = screen.getByTestId('repeat-type-select');
+    const options = within(select).getAllByRole('option');
 
-  await userEvent.clear(screen.getByLabelText(/반복 간격/));
-  await userEvent.type(screen.getByLabelText(/반복 간격/), '0');
+    expect(options).toHaveLength(4);
 
-  await userEvent.click(screen.getByRole('button', { name: /일정 추가/ }));
+    const expectedOptions = ['매일', '매주', '매월', '매년'];
 
-  expect(await screen.findByText('반복 간격을 1 이상의 숫자로 입력해주세요.')).toBeInTheDocument();
-});
-
-it('반복 유형 SELECT 옵션이 모두 표시된다.(매일/매주/매월/매년 옵션이 있어야 함)', async () => {
-  renderEventForm();
-
-  await userEvent.click(screen.getByRole('checkbox', { name: /반복 일정/ }));
-
-  const select = screen.getByTestId('repeat-type-select');
-  const options = within(select).getAllByRole('option');
-
-  expect(options).toHaveLength(4);
-
-  const expectedOptions = ['매일', '매주', '매월', '매년'];
-
-  options.forEach((option, index) => {
-    expect(option).toHaveTextContent(expectedOptions[index]);
+    options.forEach((option, index) => {
+      expect(option).toHaveTextContent(expectedOptions[index]);
+    });
   });
 });
+describe('반복 간격 설정', () => {
+  it('반복 간격을 0으로 작성 후 일정 추가 시 반복 간격을 1 이상의 숫자로 입력해주세요 문구가 뜬다', async () => {
+    renderEventForm();
+
+    await userEvent.type(screen.getByLabelText(/제목/), '제목');
+    await userEvent.type(screen.getByLabelText(/날짜/), '2024-11-13');
+    await userEvent.type(screen.getByLabelText(/시작 시간/), '09:00');
+    await userEvent.type(screen.getByLabelText(/종료 시간/), '10:00');
+    await userEvent.type(screen.getByLabelText(/설명/), '설명');
+    await userEvent.type(screen.getByLabelText(/위치/), '위치');
+    await userEvent.selectOptions(screen.getByLabelText(/카테고리/), '업무');
+
+    await userEvent.click(screen.getByRole('checkbox', { name: /반복 일정/ }));
+
+    await userEvent.clear(screen.getByLabelText(/반복 간격/));
+    await userEvent.type(screen.getByLabelText(/반복 간격/), '0');
+
+    await userEvent.click(screen.getByRole('button', { name: /일정 추가/ }));
+
+    expect(
+      await screen.findByText('반복 간격을 1 이상의 숫자로 입력해주세요.')
+    ).toBeInTheDocument();
+  });
+});
+describe('반복 일정 표시', () => {});
+describe('반복 종료', () => {});
+describe('반복 일정 단일 수정', () => {});
+describe('반복 일정 단일 삭제', () => {});
