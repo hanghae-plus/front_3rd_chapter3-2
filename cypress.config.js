4f992ecbced62bf314f52f4ce4dc7bdb47fb9c30
이동아 <christar99@naver.com>
2024-11-14 18:49:41 +0900

    ✨ feature: cypress 기본설정, mock service워커 public폴더에 생성.

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 export default {
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:5173/',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    testIsolation: false,
   },
 };
