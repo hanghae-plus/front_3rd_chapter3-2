a0817033fa06192b93c8df5aede53f1fa955e268
seriparkdev <parkseridev@gmail.com>
2024-11-15 02:45:04 +0900

    test: generateRepeatedEvents 함수 test 수정

--- src/__tests__/unit/eventRepeat.spec.ts
+++ src/__tests__/unit/eventRepeat.spec.ts
@@ -14,22 +14,11 @@ describe('generateRepeatedEvents', () => {
       description: '',
       location: '',
       category: '',
-      repeat: { type: 'daily', interval: 2, endDate: '2024-11-20' },
+      repeat: { type: 'daily', interval: 2, endDate: '2024-11-15' },
       notificationTime: 10,
     };
 
-    const repeatedEvents = generateRepeatedEvents({
-      id: '1',
-      title: '반복할 이벤트',
-      date: '2024-02-30',
-      startTime: '09:00',
-      endTime: '10:00',
-      description: '',
-      location: '',
-      category: '',
-      repeat: { type: 'daily', interval: 2, endDate: '2024-11-20' },
-      notificationTime: 10,
-    });
+    const repeatedEvents = generateRepeatedEvents(eventToRepeat);
 
     expect(repeatedEvents).toEqual([
       {
@@ -41,7 +30,7 @@ describe('generateRepeatedEvents', () => {
         description: '',
         location: '',
         category: '',
-        repeat: { type: 'daily', interval: 2, endDate: '2024-11-20' },
+        repeat: { type: 'daily', interval: 2, endDate: '2024-11-15' },
         notificationTime: 10,
       },
       {
@@ -53,7 +42,7 @@ describe('generateRepeatedEvents', () => {
         description: '',
         location: '',
         category: '',
-        repeat: { type: 'daily', interval: 2, endDate: '2024-11-20' },
+        repeat: { type: 'daily', interval: 2, endDate: '2024-11-15' },
         notificationTime: 10,
       },
       {
@@ -65,7 +54,7 @@ describe('generateRepeatedEvents', () => {
         description: '',
         location: '',
         category: '',
-        repeat: { type: 'daily', interval: 2, endDate: '2024-11-20' },
+        repeat: { type: 'daily', interval: 2, endDate: '2024-11-15' },
         notificationTime: 10,
       },
     ]);
@@ -81,13 +70,13 @@ describe('generateRepeatedEvents', () => {
       description: '',
       location: '',
       category: '',
-      repeat: { type: 'weekly', interval: 2, endDate: '2024-01-14' },
+      repeat: { type: 'weekly', interval: 2, endDate: '2024-12-15' },
       notificationTime: 10,
     };
 
     const repeatedEvents = generateRepeatedEvents(eventToRepeat);
 
-    expect(repeatedEvents).toEqual([
+    expect(repeatedEvents).toStrictEqual([
       {
         id: '1',
         title: '반복할 이벤트',
@@ -97,7 +86,7 @@ describe('generateRepeatedEvents', () => {
         description: '',
         location: '',
         category: '',
-        repeat: { type: 'weekly', interval: 2, endDate: '2024-01-14' },
+        repeat: { type: 'weekly', interval: 2, endDate: '2024-12-15' },
         notificationTime: 10,
       },
       {
@@ -109,7 +98,7 @@ describe('generateRepeatedEvents', () => {
         description: '',
         location: '',
         category: '',
-        repeat: { type: 'weekly', interval: 2, endDate: '2024-01-14' },
+        repeat: { type: 'weekly', interval: 2, endDate: '2024-12-15' },
         notificationTime: 10,
       },
       {
@@ -121,7 +110,7 @@ describe('generateRepeatedEvents', () => {
         description: '',
         location: '',
         category: '',
-        repeat: { type: 'weekly', interval: 2, endDate: '2024-01-14' },
+        repeat: { type: 'weekly', interval: 2, endDate: '2024-12-15' },
         notificationTime: 10,
       },
     ]);
@@ -137,7 +126,7 @@ describe('generateRepeatedEvents', () => {
       description: '',
       location: '',
       category: '',
-      repeat: { type: 'monthly', interval: 2, endDate: '2024-04-10' },
+      repeat: { type: 'monthly', interval: 2, endDate: '2025-04-10' },
       notificationTime: 10,
     };
 
@@ -153,19 +142,31 @@ describe('generateRepeatedEvents', () => {
         description: '',
         location: '',
         category: '',
-        repeat: { type: 'monthly', interval: 2, endDate: '2024-04-10' },
+        repeat: { type: 'monthly', interval: 2, endDate: '2025-04-10' },
         notificationTime: 10,
       },
       {
         id: '1',
         title: '반복할 이벤트',
-        date: '2024-01-10',
+        date: '2025-01-10',
         startTime: '09:00',
         endTime: '10:00',
         description: '',
         location: '',
         category: '',
-        repeat: { type: 'monthly', interval: 2, endDate: '2024-04-10' },
+        repeat: { type: 'monthly', interval: 2, endDate: '2025-04-10' },
+        notificationTime: 10,
+      },
+      {
+        id: '1',
+        title: '반복할 이벤트',
+        date: '2025-03-10',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '',
+        location: '',
+        category: '',
+        repeat: { type: 'monthly', interval: 2, endDate: '2025-04-10' },
         notificationTime: 10,
       },
     ]);
@@ -203,7 +204,7 @@ describe('generateRepeatedEvents', () => {
       {
         id: '1',
         title: '반복할 이벤트',
-        date: '2026-01-10',
+        date: '2026-11-10',
         startTime: '09:00',
         endTime: '10:00',
         description: '',
@@ -215,7 +216,7 @@ describe('generateRepeatedEvents', () => {
       {
         id: '1',
         title: '반복할 이벤트',
-        date: '2028-01-10',
+        date: '2028-11-10',
         startTime: '09:00',
         endTime: '10:00',
         description: '',
@@ -227,7 +228,7 @@ describe('generateRepeatedEvents', () => {
     ]);
   });
 
-  it('종료일이 지정되지 않은 경우 종료 없이 반복된 이벤트가 생성된다.', () => {
+  it('종료일이 지정되지 않은 경우 2025-06-30까지의 반복 일정 배열을 반환한다.', () => {
     const eventToRepeat: Event = {
       id: '1',
       title: '반복할 이벤트',
@@ -237,7 +238,7 @@ describe('generateRepeatedEvents', () => {
       description: '',
       location: '',
       category: '',
-      repeat: { type: 'yearly', interval: 10 },
+      repeat: { type: 'monthly', interval: 3 },
       notificationTime: 10,
     };
 
@@ -245,40 +246,173 @@ describe('generateRepeatedEvents', () => {
 
     expect(repeatedEvents).toEqual([
       {
-        id: '1',
-        title: '반복할 이벤트',
+        category: '',
         date: '2024-11-10',
-        startTime: '09:00',
-        endTime: '10:00',
         description: '',
+        endTime: '10:00',
+        id: '1',
         location: '',
-        category: '',
-        repeat: { type: 'yearly', interval: 10 },
         notificationTime: 10,
+        repeat: {
+          interval: 3,
+          type: 'monthly',
+        },
+        startTime: '09:00',
+        title: '반복할 이벤트',
       },
       {
-        id: '1',
-        title: '반복할 이벤트',
-        date: '2034-11-10',
-        startTime: '09:00',
-        endTime: '10:00',
-        description: '',
-        location: '',
         category: '',
-        repeat: { type: 'yearly', interval: 10 },
+        date: '2025-02-10',
+        description: '',
+        endTime: '10:00',
+        id: '1',
+        location: '',
         notificationTime: 10,
+        repeat: {
+          interval: 3,
+          type: 'monthly',
+        },
+        startTime: '09:00',
+        title: '반복할 이벤트',
       },
       {
-        id: '1',
-        title: '반복할 이벤트',
-        date: '2044-11-10',
-        startTime: '09:00',
-        endTime: '10:00',
-        description: '',
-        location: '',
         category: '',
-        repeat: { type: 'yearly', interval: 10 },
+        date: '2025-05-10',
+        description: '',
+        endTime: '10:00',
+        id: '1',
+        location: '',
         notificationTime: 10,
+        repeat: {
+          interval: 3,
+          type: 'monthly',
+        },
+        startTime: '09:00',
+        title: '반복할 이벤트',
+      },
+    ]);
+  });
+
+  it('31일에 반복되는 일정일 경우 31일이 존재하지 않는 달에는 달의 마지막 날로 조정된다.', () => {
+    const eventToRepeat: Event = {
+      id: '1',
+      title: '반복할 이벤트',
+      date: '2024-10-31',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '',
+      location: '',
+      category: '',
+      repeat: { type: 'monthly', interval: 1, endDate: '2025-02-01' },
+      notificationTime: 10,
+    };
+
+    const repeatedEvents = generateRepeatedEvents(eventToRepeat);
+
+    expect(repeatedEvents).toEqual([
+      {
+        category: '',
+        date: '2024-10-31',
+        description: '',
+        endTime: '10:00',
+        id: '1',
+        location: '',
+        notificationTime: 10,
+        repeat: { type: 'monthly', interval: 1, endDate: '2025-02-01' },
+        startTime: '09:00',
+        title: '반복할 이벤트',
+      },
+      {
+        category: '',
+        date: '2024-11-30',
+        description: '',
+        endTime: '10:00',
+        id: '1',
+        location: '',
+        notificationTime: 10,
+        repeat: { type: 'monthly', interval: 1, endDate: '2025-02-01' },
+        startTime: '09:00',
+        title: '반복할 이벤트',
+      },
+      {
+        category: '',
+        date: '2024-12-30',
+        description: '',
+        endTime: '10:00',
+        id: '1',
+        location: '',
+        notificationTime: 10,
+        repeat: { type: 'monthly', interval: 1, endDate: '2025-02-01' },
+        startTime: '09:00',
+        title: '반복할 이벤트',
+      },
+      {
+        category: '',
+        date: '2025-01-30',
+        description: '',
+        endTime: '10:00',
+        id: '1',
+        location: '',
+        notificationTime: 10,
+        repeat: { type: 'monthly', interval: 1, endDate: '2025-02-01' },
+        startTime: '09:00',
+        title: '반복할 이벤트',
+      },
+    ]);
+  });
+
+  it('2월 29일 기준 매년 반복되는 일정을 생성할 경우 평년일 때는 2월의 28일로 조정된다.', () => {
+    const eventToRepeat: Event = {
+      id: '1',
+      title: '반복할 이벤트',
+      date: '2024-02-29',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '',
+      location: '',
+      category: '',
+      repeat: { type: 'yearly', interval: 1, endDate: '2026-03-01' },
+      notificationTime: 10,
+    };
+
+    const repeatedEvents = generateRepeatedEvents(eventToRepeat);
+
+    expect(repeatedEvents).toEqual([
+      {
+        category: '',
+        date: '2024-02-29',
+        description: '',
+        endTime: '10:00',
+        id: '1',
+        location: '',
+        notificationTime: 10,
+        repeat: { type: 'yearly', interval: 1, endDate: '2026-03-01' },
+        startTime: '09:00',
+        title: '반복할 이벤트',
+      },
+      {
+        category: '',
+        date: '2025-02-28',
+        description: '',
+        endTime: '10:00',
+        id: '1',
+        location: '',
+        notificationTime: 10,
+        repeat: { type: 'yearly', interval: 1, endDate: '2026-03-01' },
+        startTime: '09:00',
+        title: '반복할 이벤트',
+      },
+      {
+        category: '',
+        date: '2026-02-28',
+        description: '',
+        endTime: '10:00',
+        id: '1',
+        location: '',
+        notificationTime: 10,
+        repeat: { type: 'yearly', interval: 1, endDate: '2026-03-01' },
+        startTime: '09:00',
+        title: '반복할 이벤트',
       },
     ]);
   });
