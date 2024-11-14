de112319544da7f74d0ae659f6263824557b44d1
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-11-15 06:38:08 +0900

    ✨ feat :: 스토리북 기본 개발 세팅 preview, main.js 구현

---
+++ .storybook/preview.tsx
@@ -0,0 +1,23 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import type { Preview } from '@storybook/react';
+import React from "react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ChakraProvider>
+        <Story />
+      </ChakraProvider>
+    ),
+  ],
+};
+
+export default preview;
