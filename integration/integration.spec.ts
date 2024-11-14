8bd74e4998d64e4bc0c74fab260a525c19871f7c
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-11-15 06:15:48 +0900

    ✅ tests :: Playwright로 통합테스트 코드 작성을 위한 초기 세팅

---
+++ integration/integration.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+import { exec } from 'child_process';
+import waitOn from 'wait-on';
+import { promisify } from 'util';
+
+const waitOnPromise = promisify(waitOn);
+
+let serverProcess;
+
+test.describe.serial('통합 테스트', () => {
+  test.beforeAll(async () => {
+    serverProcess = exec('pnpm dev');
+
+    await waitOnPromise({
+      resources: ['http://localhost:5173'],
+      timeout: 10000,
+    });
+  });
+
+
+  test.afterAll(() => {
+    if (serverProcess) {
+      serverProcess.kill();
+    }
+  });
+});
