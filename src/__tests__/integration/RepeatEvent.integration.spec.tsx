1d4dafba0419b283e15bd2a32802903552e3478f
seriparkdev <parkseridev@gmail.com>
2024-11-15 05:53:50 +0900

    test: 반복 일정에 대한 통합 테스트 작성

--- src/__tests__/integration/RepeatEvent.integration.spec.tsx
+++ src/__tests__/integration/RepeatEvent.integration.spec.tsx
@@ -1,9 +1,15 @@
-import { ReactElement } from 'react';
-import { userEvent } from '@testing-library/user-event';
-import { render, screen, within } from '@testing-library/react';
 import { ChakraProvider } from '@chakra-ui/react';
-import App from '../../App.tsx';
+import { render, screen, within } from '@testing-library/react';
+import { UserEvent, userEvent } from '@testing-library/user-event';
+import { ReactElement } from 'react';
 import { expect } from 'vitest';
+
+import {
+  setupMockHandlerCreation,
+  setupMockHandlerDeletion,
+  setupMockHandlerUpdating,
+} from '../../__mocks__/handlersUtils.ts';
+import App from '../../App.tsx';
 import { Event } from '../../types.ts';
 
 const setup = (element: ReactElement) => {
@@ -25,14 +31,17 @@ const saveSchedule = async (user: UserEvent, form: Omit<Event, 'id' | 'notificat
   await user.type(screen.getByLabelText('위치'), location);
   await user.selectOptions(screen.getByLabelText('카테고리'), category);
   await user.selectOptions(screen.getByLabelText('반복 유형'), repeat.type);
-  await user.selectOptions(screen.getByLabelText('반복 간격'), repeat.interval);
-  await user.selectOptions(screen.getByLabelText('반복 종료일'), repeat.endDate);
+  await user.clear(screen.getByLabelText(/반복 간격/));
+  await user.type(screen.getByLabelText(/반복 간격/), String(repeat.interval));
+  await user.type(screen.getByLabelText(/반복 종료일/), String(repeat.endDate));
 
   await user.click(screen.getByTestId('event-submit-button'));
 };
 
 describe('반복 일정 등록/조회', () => {
   it('일정 반복 설정 시 필수 입력 필드들이 표출된다.', async () => {
+    setupMockHandlerCreation();
+
     setup(<App />);
 
     const eventForm = screen.getByTestId('event-form');
@@ -45,30 +54,103 @@ describe('반복 일정 등록/조회', () => {
   it('일정 반복 설정 해제 시 필수 입력 필드들이 사라진다.', async () => {
     const { user } = setup(<App />);
 
-    await user.click(screen.getByRole('checkbox', { name: '반복 일정' }));
+    await user.click(screen.getByRole('checkbox', { name: /반복 일정/ }));
 
     const eventForm = screen.getByTestId('event-form');
 
-    expect(within(eventForm).getByLabelText('반복 유형')).not.toBeInTheDocument();
-    expect(within(eventForm).getByLabelText('반복 간격')).not.toBeInTheDocument();
-    expect(within(eventForm).getByLabelText('반복 종료일')).not.toBeInTheDocument();
+    expect(within(eventForm).queryByText(/반복 유형/)).not.toBeInTheDocument();
+    expect(within(eventForm).queryByText(/반복 간격/)).not.toBeInTheDocument();
+    expect(within(eventForm).queryByText(/반복 종료일/)).not.toBeInTheDocument();
   });
 
-  it('과거 날짜로 종료일 설정 시 에러가 발생한다.', () => {});
+  it('반복 등록 시 일정 뷰에서 아이콘과 함께 표시된다.', async () => {
+    setupMockHandlerCreation();
 
-  it('등록하려는 날짜보다 월의 수가 더 적을 때 경고 문구가 뜨고 해당 월의 마지막 날에 일정이 등록된다.', () => {});
+    const { user } = setup(<App />);
 
-  it('반복 일정일 경우 일정 뷰에서 아이콘과 함께 표시된다.', () => {});
+    await saveSchedule(user, {
+      title: '반복할 일정',
+      date: '2024-10-01',
+      startTime: '10:00',
+      endTime: '12:00',
+      description: 'test',
+      location: 'test',
+      category: '업무',
+      repeat: { type: 'weekly', interval: 1, endDate: '2024-10-30' },
+    });
+
+    const monthView = screen.getByTestId('month-view');
+    expect(within(monthView).getAllByText(/반복할 일정/)).toHaveLength(5);
+    expect(within(monthView).getAllByLabelText('Repeat Event')).toHaveLength(5);
+  });
 });
 
 describe('반복 일정 수정/삭제', () => {
-  it('반복 일정 중 특정 날짜만 수정할 수 있다.', () => {});
+  it('반복 일정 설정 해제 시 단일 일정으로 수정되고 일정 뷰에서도 아이콘이 사라진다.', async () => {
+    setupMockHandlerUpdating([
+      {
+        id: '1',
+        title: '반복할 일정',
+        date: '2024-10-01',
+        startTime: '10:00',
+        endTime: '12:00',
+        description: 'test',
+        location: 'test',
+        category: '업무',
+        repeat: { type: 'yearly', interval: 1, endDate: '2025-10-30' },
+        notificationTime: 10,
+      },
+    ]);
 
-  it('반복 일정 전체를 수정할 수 있다.', () => {});
+    const { user } = setup(<App />);
 
-  it('반복 일정 설정 해제 시 단일 일정으로 수정되고 일정 뷰에서도 아이콘이 사라진다.', () => {});
+    const eventList = screen.getByTestId('event-list');
+    const monthView = screen.getByTestId('month-view');
 
-  it('반복 일정일 경우 일정 뷰에서 아이콘과 함께 표시된다.', () => {});
+    expect(await within(monthView).findAllByLabelText('Repeat Event')).toHaveLength(1);
 
-  it('반복 일정 삭제 시 해당 일정만 삭제된다.', () => {});
+    await user.click(await within(eventList).findByLabelText('Edit event'));
+    await user.click(screen.getByTestId('event-submit-button'));
+
+    expect(within(monthView).queryByLabelText('Repeat Event')).not.toBeInTheDocument();
+  });
+
+  it('반복 일정 삭제 시 선택한 해당 일정만 삭제된다.', async () => {
+    setupMockHandlerDeletion([
+      {
+        id: '1',
+        title: '반복할 일정',
+        date: '2024-10-01',
+        startTime: '10:00',
+        endTime: '12:00',
+        description: 'test',
+        location: 'test',
+        category: '업무',
+        repeat: { type: 'weekly', interval: 1, endDate: '2024-10-12' },
+        notificationTime: 10,
+      },
+      {
+        id: '2',
+        title: '반복할 일정',
+        date: '2024-10-08',
+        startTime: '10:00',
+        endTime: '12:00',
+        description: 'test',
+        location: 'test',
+        category: '업무',
+        repeat: { type: 'weekly', interval: 1, endDate: '2024-10-12' },
+        notificationTime: 10,
+      },
+    ]);
+
+    const { user } = setup(<App />);
+
+    const monthView = screen.getByTestId('month-view');
+    expect(await within(monthView).findAllByLabelText('Repeat Event')).toHaveLength(2);
+
+    const allDeleteButton = await screen.findAllByLabelText('Delete event');
+    await user.click(allDeleteButton[0]);
+
+    expect(within(monthView).getAllByLabelText('Repeat Event')).toHaveLength(1);
+  });
 });
