282b50a7c727ed61c106cae65266c023197df168
cruellaDev <cruellaDevLee@gmail.com>
2024-11-14 20:30:26 +0900

    * feat: eventUtils - generateRepeatedEvents 생성

--- src/utils/eventUtils.ts
+++ src/utils/eventUtils.ts
@@ -1,5 +1,10 @@
-import { Event } from '../types';
-import { getWeekDates, isDateInRange } from './dateUtils';
+import { Event, EventForm } from '../types';
+import {
+  formatDate,
+  getRemainingDatesOfRepeatedEvent,
+  getWeekDates,
+  isDateInRange,
+} from './dateUtils';
 
 function filterEventsByDateRange(events: Event[], start: Date, end: Date): Event[] {
   return events.filter((event) => {
@@ -47,4 +52,22 @@ export function getFilteredEvents(
   }
 
   return searchedEvents;
+}
+
+export function generateRepeatedEvents(event: Event): EventForm[] {
+  const dates: Date[] = getRemainingDatesOfRepeatedEvent(event.date, event.repeat);
+  const { title, startTime, endTime, description, location, category, repeat, notificationTime } =
+    event;
+  const events = dates.map((date) => ({
+    title,
+    startTime,
+    endTime,
+    description,
+    location,
+    category,
+    repeat,
+    notificationTime,
+    date: formatDate(date),
+  }));
+  return events;
 }
