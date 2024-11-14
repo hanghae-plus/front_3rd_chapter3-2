34c1fcc80efbf5f53732df3191a4eeb34ea473f1
Bong <borobong230@gmail.com>
2024-11-15 05:52:33 +0900

    :bug: [Fix] eslint 오류 해결 #main

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      return config;
-    },
     supportFile: false,
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
