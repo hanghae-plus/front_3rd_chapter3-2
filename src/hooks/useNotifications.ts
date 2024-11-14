fdff04e49fa8fc7ede72364808636d47950c8f05
JayeHa <jwha94@gmail.com>
2024-11-14 10:47:38 +0900

    chore: useNotifications 주석 추가

--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -9,15 +9,19 @@ export const useNotifications = (events: Event[]) => {
   const [notifications, setNotifications] = useState<Notification[]>([]);
   const [notifiedEvents, setNotifiedEvents] = useState<EventId[]>([]);
 
+  /** 다가오는 이벤트를 확인하고 알림을 설정하는 함수 */
   const checkUpcomingEvents = () => {
     const now = new Date();
     const upcomingEvents = getUpcomingEvents(events, now, notifiedEvents);
 
     const upcomingEventIds = upcomingEvents.map(({ id }) => id);
+
+    // 이전에 알림한 이벤트 목록과 다가오는 이벤트 ID 목록이 같으면 변경 사항이 없으므로 종료
     if (arraysEqual(notifiedEvents, upcomingEventIds)) {
       return;
     }
 
+    // 새로운 알림 메세지 추가
     setNotifications((prev) => [
       ...prev,
       ...upcomingEvents.map((event) => ({
@@ -26,9 +30,11 @@ export const useNotifications = (events: Event[]) => {
       })),
     ]);
 
-    setNotifiedEvents((prev) => [...prev, ...upcomingEvents.map(({ id }) => id)]);
+    // 알림이 발송된 이벤트 id 목록 업데이트
+    setNotifiedEvents((prev) => [...prev, ...upcomingEventIds]);
   };
 
+  /** 특정 인덱스의 알림을 제거하는 함수 */
   const removeNotification = (index: number) => {
     setNotifications((prev) => prev.filter((_, i) => i !== index));
   };
