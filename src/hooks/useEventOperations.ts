8575e69cb1b4a52e1435af1aaf2097857d906aa8
cruellaDev <cruellaDevLee@gmail.com>
2024-11-15 02:30:03 +0900

    * feat: useEventOperations - addEventWithNewRepeatedInfo, updateEventWithNewRepeatedInfo 생성

--- src/hooks/useEventOperations.ts
+++ src/hooks/useEventOperations.ts
@@ -66,6 +66,111 @@ export const useEventOperations = (editing: boolean, onSave?: () => void) => {
     }
   };
 
+  const addEventWithNewRepeatedInfo = async (
+    eventData: Event | EventForm,
+    eventListData: EventForm[]
+  ) => {
+    try {
+      // event add
+      let response;
+      response = await fetch('/api/events', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(eventData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to save event list');
+      }
+
+      const newEvent = (await response.json()) as Event;
+
+      // event list add
+      response = await fetch('/api/events-list', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(
+          eventListData.map((item) => ({ ...item, repeat: { ...item.repeat, id: newEvent.id } }))
+        ),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to save event list');
+      }
+
+      await fetchEvents();
+      onSave?.();
+      toast({
+        title: '일정이 등록되었습니다.',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      });
+    } catch (error) {
+      console.error('Error saving event:', error);
+      toast({
+        title: '반복 일정 추가 실패',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const updateEventWithNewRepeatedInfo = async (
+    eventData: Event | EventForm,
+    eventListData: EventForm[]
+  ) => {
+    try {
+      // event update
+      let response;
+      response = await fetch(`/api/events/${(eventData as Event).id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(eventData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to save event list');
+      }
+
+      const updatedEvent = (await response.json()) as Event;
+
+      // event list add
+      response = await fetch('/api/events-list', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(
+          eventListData.map((item) => ({
+            ...item,
+            repeat: { ...item.repeat, id: updatedEvent.id },
+          }))
+        ),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to save event list');
+      }
+
+      await fetchEvents();
+      onSave?.();
+      toast({
+        title: '일정이 수정되었습니다.',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      });
+    } catch (error) {
+      console.error('Error saving event:', error);
+      toast({
+        title: '반복 일정 수정 실패',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
   const deleteEvent = async (id: string) => {
     try {
       const response = await fetch(`/api/events/${id}`, { method: 'DELETE' });
@@ -106,5 +211,12 @@ export const useEventOperations = (editing: boolean, onSave?: () => void) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return { events, fetchEvents, saveEvent, deleteEvent };
+  return {
+    events,
+    fetchEvents,
+    saveEvent,
+    deleteEvent,
+    addEventWithNewRepeatedInfo,
+    updateEventWithNewRepeatedInfo,
+  };
 };
